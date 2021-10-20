8a58e78b78c12693f3d4ea28b17bea6a0343b712
Mikhail Teslya <televonvea@gmail.com>
2021-10-20 11:57:22 +0300

    slider

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,11 +3,11 @@ const mainSlider = $('#main-slider');
 mainSlider.on('init', function(event, slick) {
   const slideCount = slick.slideCount;
   $('#count-end').text('0' + slideCount);
-})
+});
 mainSlider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
   const nextSlideInc = nextSlide + 1;
   $('#count-start').text('0' + nextSlideInc);
-})
+});
 mainSlider.slick({
   fade: true,
   dots: true,
@@ -36,5 +36,30 @@ inputWraps.each((idx, item) => {
       $label.removeClass('active');
     }
   })
+});
 
-})
+// Слайдеры в секции сравнения
+const comparisonSliders = $('.comparison-slider-wrap');
+
+comparisonSliders.each((idx, item) => {
+  const $item = $(item);
+  const $slider = $item.find('.comparison-slider');
+
+  $slider.on('init', function(event, slick) {
+    const slideCount = slick.slideCount;
+    $item.find('.comparison-slider-controlls-count__end').text('0' + slideCount);
+  });
+  $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    const nextSlideInc = nextSlide + 1;
+    $item.find('.comparison-slider-controlls-count__start').text('0' + nextSlideInc);
+  });
+
+  $slider.slick({
+    fade: true,
+    infinite: true,
+    speed: 600,
+    appendArrows: $item.find('.comparison-slider-controlls-arrows'),
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+});
